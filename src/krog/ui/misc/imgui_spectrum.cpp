42b33ed0ba7a85e5488bf7b43eb85f94729fc988
taubedonner <info@xxcf.cf>
2024-02-05 08:34:02 +0300

    Added extra glyphs for primary font texture

--- src/krog/ui/misc/imgui_spectrum.cpp
+++ src/krog/ui/misc/imgui_spectrum.cpp
@@ -17,20 +17,20 @@ namespace ImGui {
     namespace Spectrum {
 
         static Props props;
-        static ImFont* fonts[2];
+        static ImFont *fonts[2];
 
-		 bool operator==(ImVec4& a, ImVec4& b) {
+        bool operator==(ImVec4 &a, ImVec4 &b) {
             return (ColorConvertFloat4ToU32(a) & ~IM_COL32_A_MASK) == (ColorConvertFloat4ToU32(b) & ~IM_COL32_A_MASK);
         }
 
         // This came from imgui_demo.cpp and adapted for Adobe Spectrum color palette (currently dark-only)
-		 void ShowStyleEditor(bool* p_open) {
+        void ShowStyleEditor(bool *p_open) {
             if (ImGui::Begin("Spectrum Color Editor", p_open)) {
-                ImGuiStyle& style = ImGui::GetStyle();
+                ImGuiStyle &style = ImGui::GetStyle();
                 static ImGuiStyle ref_saved_style = style;
                 auto ref = &ref_saved_style;
                 static int selectedPalette = 0;
-                static const char* paletteNames[2] = {"Darkest", "Light"};
+                static const char *paletteNames[2] = {"Darkest", "Light"};
                 static auto colorPalette = &Colors::Darkest.Values;
 
                 static int output_dest = 0;
@@ -42,15 +42,18 @@ namespace ImGui {
                         ImGui::LogToTTY();
                     ImGui::LogText("ImVec4* colors = ImGui::GetStyle().Colors;\n");
                     for (int i = 0; i < ImGuiCol_COUNT; i++) {
-                        const ImVec4& col = style.Colors[i];
-                        const char* name = ImGui::GetStyleColorName(i);
+                        const ImVec4 &col = style.Colors[i];
+                        const char *name = ImGui::GetStyleColorName(i);
                         auto foundPreview = std::find_if(Colors::Darkest.Values.begin(), Colors::Darkest.Values.end(),
                                                          [&](auto pl) { return pl.second == style.Colors[i]; });
                         if (!output_only_modified || memcmp(&col, &ref->Colors[i], sizeof(ImVec4)) != 0) {
                             std::string preview = (foundPreview == Colors::Darkest.Values.end())
-                                                  ? fmt::format("colors[ImGuiCol_{}]{:>{}} ImVec4({:.2f}f, {:.2f}f, {:.2f}f, {:.2f}f);\n", name, "=",
-                                                                23 - (int) strlen(name), col.x, col.y, col.z, col.w)
-                                                  : fmt::format("colors[ImGuiCol_{}]{:>{}} Darkest(DARKEST_{});\n", name, "=", 23 - (int) strlen(name),
+                                                  ? fmt::format(
+                                            "colors[ImGuiCol_{}]{:>{}} ImVec4({:.2f}f, {:.2f}f, {:.2f}f, {:.2f}f);\n",
+                                            name, "=",
+                                            23 - (int) strlen(name), col.x, col.y, col.z, col.w)
+                                                  : fmt::format("colors[ImGuiCol_{}]{:>{}} Darkest(DARKEST_{});\n",
+                                                                name, "=", 23 - (int) strlen(name),
                                                                 foundPreview->first);
                             ImGui::LogText("%s", preview.c_str());
                             KR_DEBUG(preview);
@@ -85,20 +88,21 @@ namespace ImGui {
                 ImGui::BeginChild("##colors", ImVec2(0, 0), true, ImGuiWindowFlags_NavFlattened);
                 ImGui::PushItemWidth(-160);
                 for (int i = 0; i < ImGuiCol_COUNT; i++) {
-                    const char* name = ImGui::GetStyleColorName(i);
+                    const char *name = ImGui::GetStyleColorName(i);
                     if (!filter.PassFilter(name))
                         continue;
                     ImGui::PushID(i);
                     auto cid = fmt::format("##colorb_{}", i);
 
 
-                    auto foundPreview = std::find_if(colorPalette->begin(), colorPalette->end(), [&](auto pl) { return pl.second == style.Colors[i]; });
+                    auto foundPreview = std::find_if(colorPalette->begin(), colorPalette->end(),
+                                                     [&](auto pl) { return pl.second == style.Colors[i]; });
                     auto preview = foundPreview == colorPalette->end() ? "" : foundPreview->first;
 
                     ImGui::SetNextItemWidth(180);
                     if (ImGui::BeginCombo("##paletteSelector", preview)) {
                         size_t cplid = 0;
-                        for (auto& pl: *colorPalette) {
+                        for (auto &pl: *colorPalette) {
                             auto pcid = fmt::format("{}_cpl_{}", cid, cplid);
                             ImGui::ColorButton(pcid.c_str(), pl.second);
                             ImGui::SameLine();
@@ -128,9 +132,9 @@ namespace ImGui {
             }
         }
 
-		 void Init(float fontSize) {
-            auto& io = ImGui::GetIO();
-            auto& style = ImGui::GetStyle();
+        void Init(float fontSize) {
+            auto &io = ImGui::GetIO();
+            auto &style = ImGui::GetStyle();
 
             style.FramePadding = {8.0f, 3.0f};
 
@@ -154,25 +158,37 @@ namespace ImGui {
             props.FontSize = fontSize;
             props.Padding = style.FramePadding;
             props.WindowPadding = style.WindowPadding;
-            props.IconSize = { 20.0f, props.FontSize };
-            props.ButtonMinSize = { ((props.Padding.x) + props.IconSize.x), ((props.Padding.y * 2.0f) + props.IconSize.y) };
+            props.IconSize = {20.0f, props.FontSize};
+            props.ButtonMinSize = {((props.Padding.x) + props.IconSize.x),
+                                   ((props.Padding.y * 2.0f) + props.IconSize.y)};
 
             // Default font
+
+            static const ImWchar glyphRanges[] = {
+                    0x0020, 0x00FF, // Basic Latin + Latin Supplement
+                    0x0370, 0x03FF, // Greek and Coptic
+                    0x0400, 0x052F, // Cyrillic + Cyrillic Supplement
+                    0x2DE0, 0x2DFF, // Cyrillic Extended-A
+                    0xA640, 0xA69F, // Cyrillic Extended-B
+                    0x2070, 0x209F, // Superscripts and Subscripts
+                    0x2100, 0x214F, // Letter-like Symbols
+                    0,
+            };
 
             ImFontConfig config;
             io.Fonts->AddFontFromMemoryCompressedTTF(
                     TTF_SFPROTEXT_compressed_data,
                     TTF_SFPROTEXT_compressed_size,
                     props.FontSize, &config,
-                    io.Fonts->GetGlyphRangesCyrillic());
+                    glyphRanges);
 
             ImFontConfig config2;
             config2.MergeMode = true;
             config2.GlyphOffset = {-3.0f, 3.25f};
             config2.GlyphMinAdvanceX = props.IconSize.x;
             config2.GlyphMaxAdvanceX = props.IconSize.x;
-            static const ImWchar iconRanges[] = { ICON_MIN_CI, ICON_MAX_CI, 0 };
-            fonts[(int)Font::Default] = io.Fonts->AddFontFromMemoryCompressedTTF(
+            static const ImWchar iconRanges[] = {ICON_MIN_CI, ICON_MAX_CI, 0};
+            fonts[(int) Font::Default] = io.Fonts->AddFontFromMemoryCompressedTTF(
                     TTF_CODICON_compressed_data,
                     TTF_CODICON_compressed_size,
                     props.FontSize, &config2,
@@ -181,14 +197,14 @@ namespace ImGui {
             // Large font
             ImFontConfig config3;
             config3.GlyphOffset = {-1.0f, -2.0f};
-            fonts[(int)Font::Large] = io.Fonts->AddFontFromMemoryCompressedTTF(
+            fonts[(int) Font::Large] = io.Fonts->AddFontFromMemoryCompressedTTF(
                     TTF_CODICON_compressed_data,
                     TTF_CODICON_compressed_size,
                     props.FontSize * 2.0f, &config3,
                     iconRanges);
         }
 
-		 void StyleColorsDark() {
+        void StyleColorsDark() {
             using namespace Colors;
 
             auto colors = ImGui::GetStyle().Colors;
@@ -254,7 +270,7 @@ namespace ImGui {
             props.IsDark = true;
         }
 
-		 void StyleColorsLight() {
+        void StyleColorsLight() {
             using namespace Colors;
 
             auto colors = ImGui::GetStyle().Colors;
@@ -320,21 +336,21 @@ namespace ImGui {
             props.IsDark = false;
         }
 
-		 const Props& GetProps() {
+        const Props &GetProps() {
             return props;
         }
 
-		 ImFont* GetFont(Font font) {
+        ImFont *GetFont(Font font) {
             auto idx = static_cast<int>(font);
             return fonts[idx];
         }
 
     } // ImGui::Spectrum
 
-	 bool ColoredButton(const char* label, const ImVec4& bgColor, const ImVec4& fgColor, const ImVec2& size) {
+    bool ColoredButton(const char *label, const ImVec4 &bgColor, const ImVec4 &fgColor, const ImVec2 &size) {
         //ImGuiStyle& style = GetStyle();
-        auto bgHoveredColor = ImVec4{ bgColor.x - 0.1f, bgColor.y - 0.1f, bgColor.z - 0.1f, bgColor.w };
-        auto bgActiveColor = ImVec4{ bgColor.x - 0.2f, bgColor.y - 0.2f, bgColor.z - 0.2f, bgColor.w };
+        auto bgHoveredColor = ImVec4{bgColor.x - 0.1f, bgColor.y - 0.1f, bgColor.z - 0.1f, bgColor.w};
+        auto bgActiveColor = ImVec4{bgColor.x - 0.2f, bgColor.y - 0.2f, bgColor.z - 0.2f, bgColor.w};
 
         PushStyleColor(ImGuiCol_Button, bgColor);
         PushStyleColor(ImGuiCol_ButtonHovered, bgHoveredColor);
@@ -346,8 +362,8 @@ namespace ImGui {
         return pressed;
     }
 
-	 bool DisablingButton(const char* label, bool disabled, const ImVec2& size) {
-        ImGuiStyle& style = GetStyle();
+    bool DisablingButton(const char *label, bool disabled, const ImVec2 &size) {
+        ImGuiStyle &style = GetStyle();
 
         if (disabled) {
             auto btnColor = style.Colors[ImGuiCol_TextDisabled];
@@ -367,10 +383,11 @@ namespace ImGui {
         return pressed;
     }
 
-	 bool DisablingColoredButton(const char* label, const ImVec4& bgColor, const ImVec4& fgColor, bool disabled, const ImVec2& size) {
-        ImGuiStyle& style = GetStyle();
-        auto bgHoveredColor = ImVec4{ bgColor.x - 0.1f, bgColor.y - 0.1f, bgColor.z - 0.1f, bgColor.w };
-        auto bgActiveColor = ImVec4{ bgColor.x - 0.2f, bgColor.y - 0.2f, bgColor.z - 0.2f, bgColor.w };
+    bool DisablingColoredButton(const char *label, const ImVec4 &bgColor, const ImVec4 &fgColor, bool disabled,
+                                const ImVec2 &size) {
+        ImGuiStyle &style = GetStyle();
+        auto bgHoveredColor = ImVec4{bgColor.x - 0.1f, bgColor.y - 0.1f, bgColor.z - 0.1f, bgColor.w};
+        auto bgActiveColor = ImVec4{bgColor.x - 0.2f, bgColor.y - 0.2f, bgColor.z - 0.2f, bgColor.w};
         auto bc = disabled ? style.Colors[ImGuiCol_ScrollbarGrab] : bgColor;
         auto fc = disabled ? style.Colors[ImGuiCol_TextDisabled] : fgColor;
 
@@ -388,9 +405,9 @@ namespace ImGui {
         return pressed;
     }
 
-  	 void SeparatorV() {
-        ImGuiContext& g = *GImGui;
-        ImGuiWindow* window = g.CurrentWindow;
+    void SeparatorV() {
+        ImGuiContext &g = *GImGui;
+        ImGuiWindow *window = g.CurrentWindow;
         if (window->SkipItems) return;
         ImGuiSeparatorFlags flags = ImGuiSeparatorFlags_Vertical;
         SeparatorEx(flags, 1.0f);
@@ -400,10 +417,12 @@ namespace ImGui {
 
 namespace ImPlot {
 
-	 void PlotTextColored(const ImVec4& color, const char* text, double x, double y, const ImVec2& pixel_offset, ImPlotTextFlags flags) {
+    void PlotTextColored(const ImVec4 &color, const char *text, double x, double y, const ImVec2 &pixel_offset,
+                         ImPlotTextFlags flags) {
         ImPlot::PushStyleColor(ImPlotCol_InlayText, color);
         PlotText(text, x, y, pixel_offset, flags);
-        ImPlot:PopStyleColor();
+        ImPlot:
+        PopStyleColor();
     }
 
 } // ImPlot
